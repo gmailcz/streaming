7b2f5212666e123abaf8a8423f6d32173a6105f2
gmailcz <49840688+gmailcz@users.noreply.github.com>
2019-05-27 04:01:29 +0300

    Create yalla-shoot-trend.js

---
+++ yalla-shoot-trend.js
@@ -0,0 +1,12 @@
+window.mcwidget = {"appId":"532160876956612","pageId":"2270134409737928","widgets":[{"widget_id":6139568,"page_id":"2270134409737928","widget_type":"button","status":"active","name":"Example Popup","data":{"feed_comment_settings":[],"main":{"ctaText":"SUBSCRIBE"}}},{"widget_id":6139567,"page_id":"2160390037346499","widget_type":"landing","status":"active","name":"Example Landing","data":[]}],"widgetLocale":"ar_AR","defaultSize":0,"fbSDKVersion":"v3.1"};
+
+
+(function(d, s, id){
+    var host = 'manychat.com/111278';
+
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = '//' + host + '/assets/js/widget.js?' + (Math.round(+new Date/1000/1800));
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'mcwidget-core'));
